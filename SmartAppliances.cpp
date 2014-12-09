5da654e8addfa1cc18ecdd13674dc88ff0d33dcb
tristaoeast <tristaoeast@gmail.com>
2014-12-09 11:23:02 +0000

    Completed XML Spec for scenario 1

--- SmartAppliances.cpp
+++ SmartAppliances.cpp
@@ -1,16 +1,107 @@
 #include "pugixml.hpp"
-
 #include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main()
-{
-//[code_load_file
-    pugi::xml_document doc;
+using namespace std;
+using namespace pugi;
 
-    pugi::xml_parse_result result = doc.load_file("tree.xml");
 
-    std::cout << "Load result: " << result.description() << ", mesh name: " << doc.child("mesh").attribute("name").value() << std::endl;
-//]
+char* strToCharArray (string str) {
+	char * cstr = new char [str.length()+1];
+  strcpy (cstr, str.c_str());
+  return cstr;
 }
 
-// vim:et
+string getInput() {
+
+}
+
+int getIntegerFromInput(string display) {
+	int integer;
+	while(true){
+		cout << display;
+		cin >> integer;
+		if(cin.fail()) {
+			cin.clear();
+			string dummy;
+	  	cin >> dummy; // throw away garbage.
+			cout << "Invalid input type. Please select an option using one of the numbers betwen []." << endl;
+			continue;
+			}
+		else
+			return integer;
+	}
+}
+
+int main(int argc, char* argv[])
+{
+
+	xml_document doc;
+	char* docToLoad;
+
+	// cout << "argc: " << argc << endl;
+
+	if(argc > 1)
+		docToLoad = strToCharArray(argv[1]);
+	else
+		docToLoad = strToCharArray("dbspec.xml");
+
+	xml_parse_result result = doc.load_file(docToLoad);
+
+	string nerr = "No error";
+	if(nerr.compare(result.description())) 
+	{
+		cout << "ERROR: Error loading document " << docToLoad << ". Exiting." << endl;
+		exit(1);
+	}
+
+	xml_node domobus = doc.child("DomoBusSystem");
+	xml_node evtl = domobus.child("EnumValueTypeList");
+	xml_node evt = domobus.child("EnumValueType");
+
+
+
+	cout << endl << "Welcome to DomoBusSystem " << "Project : " << domobus.attribute("Name").value() << " - " << domobus.attribute("Type").value() << " Simulator - Version: " << domobus.attribute("Version").value() << endl;
+
+	int optionSelected; 
+	while(true) {
+		string display = "\n[1] DomoBusSystem Manager\n[2] DomoBusSystem Simulator\n\n[0] Exit\nSelect an option: ";
+		optionSelected = getIntegerFromInput(display);
+
+		if(optionSelected == 1) {
+			// cout << "ENTREI !!!!\n";
+			string display1 ;
+		} 
+
+		else if(optionSelected == 2) {
+			
+		} 
+
+		else if(optionSelected == 0) {
+			exit(0);
+		}
+
+		else {
+			cout << "Wrong option selected. Please select an option using one of the numbers betwen []." << endl;
+		}
+
+	}
+
+
+}
+
+
+
+
+
+
+//[code_load_file
+//     xml_document doc;
+
+//     xml_parse_result result = doc.load_file("tree.xml");
+
+//     cout << "Load result: " << result.description() << ", mesh name: " << doc.child("mesh").attribute("name").value() << endl;
+// //]
